b49375e8d7f556569d64190a337e63362ec9a596
makss <makss@ASUSTG>
2023-05-06 14:59:43 +0300

    Команды

---
+++ AudioPlayer/Infrastructure/Commands/DragMoveCommand.cs
@@ -0,0 +1,22 @@
+﻿using AudioPlayer.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace AudioPlayer.Infrastructure.Commands
+{
+    internal class DragMoveCommand : Command
+    {
+        Window Window { get; set; }
+
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)
+        {
+            Window.DragMove(); //228 CCRF
+        }
+
+        internal DragMoveCommand(Window window)
+        {
+            Window = window;
+        }
+    }
+}
