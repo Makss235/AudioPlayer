c692726d044cd250afabe8ad8977cde28a85140b
makss <makss@ASUSTG>
2023-05-06 22:10:15 +0300

    Проба с тегами аудио

--- AudioPlayer/Windows/MainWindow/MenuPlaylistsColumn.cs
+++ AudioPlayer/Windows/MainWindow/MenuPlaylistsColumn.cs
@@ -1,9 +1,4 @@
 ﻿using AudioPlayer.Widgets.Playlists;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 
 namespace AudioPlayer.Windows.MainWindow
---
+++ Tests/ConsoleTest/Program.cs
@@ -0,0 +1,7 @@
+﻿TagLib.File tagFile = TagLib.File.Create("D:\\Projects\\VS_CSharp\\AudioPlayer\\Tests\\ConsoleTest\\bin\\Debug\\net6.0\\Don't Cry Tonight.mp3");
+string artist = tagFile.Tag.FirstAlbumArtist;
+string album = tagFile.Tag.Album;
+string title = tagFile.Properties.Duration.ToString("mm\\:ss");
+Console.WriteLine(title);
+Console.WriteLine(album);
+Console.WriteLine(artist);
